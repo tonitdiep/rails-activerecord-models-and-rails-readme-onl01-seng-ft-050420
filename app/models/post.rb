a28908df3154bfa5d753b8661a17b5794fdfb5f9
Toni  Diep <adiep123@gmail.com>
2020-07-12 15:02:32 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  Post.all
+  post = Post.create!(title: "My title", description: "The post description")
+  Post.last
+  def post_summary
+  # self.title + " - " + self.description
+  # expect(post.post_summary).to eq("My title - The post description")
+  # "My title - The post description"
+  Post.last.post_summary
+  end
 end
